67d5a964f2632d249031395c2cfd00e506456a2d
Shyamlal Yadav <shyyadav@radisys.com>
2024-05-02 12:29:05 +0530

    first commit

---
+++ src/components/gallery.jsx
@@ -0,0 +1,35 @@
+import { Image } from "./image";
+import React from "react";
+
+export const Gallery = (props) => {
+  return (
+    <div id="portfolio" className="text-center">
+      <div className="container">
+        <div className="section-title">
+          <h2>Gallery</h2>
+          <p>
+          "Galleries are vibrant showcases of artistic talent, cultural reflection, and community engagement, inspiring and uniting people through the transformative power of art."
+          </p>
+        </div>
+        <div className="row">
+          <div className="portfolio-items">
+            {props.data
+              ? props.data.map((d, i) => (
+                  <div
+                    key={`${d.title}-${i}`}
+                    className="col-sm-6 col-md-4 col-lg-4"
+                  >
+                    <Image
+                      title={d.title}
+                      largeImage={d.largeImage}
+                      smallImage={d.smallImage}
+                    />
+                  </div>
+                ))
+              : "Loading..."}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
